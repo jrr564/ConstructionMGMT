e9ab113664f8b410856c34e37aa6af94a4f048fe
unknown <Cli@ENG-CHEN-LP.domain.local>
2018-02-23 13:50:53 -0600

    update server.js

--- server.js
+++ server.js
@@ -14,20 +14,24 @@ var exphbs = require("express-handlebars");
 
 var port     = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'))
 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'))
 
-app.use(morgan('dev'))
 // log every request to the console
+app.use(morgan('dev'))
 
-app.use(methodOverride())
 // simulate DELETE and PUT
+app.use(methodOverride())
 
-app.use(cookieParser())
 // read cookies (needed for auth)
+app.use(cookieParser())
 
-app.use(bodyParser())
-// get information from html forms
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 // required for passport
 app.use(session({ secret: 'constructionmgmt' })); // session secret
